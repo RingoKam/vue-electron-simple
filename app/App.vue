15e62f48f5098fc8e285b46abb816c6ca383ea41
alexcheninfo <alexchen.info@gmail.com>
2016-03-21 23:09:13 +0800

    Remove unnecessary code

--- app/App.vue
+++ app/App.vue
@@ -5,11 +5,6 @@
 <script>
 import Hello from './components/Hello'
 
-// Define Electron objects like this:
-// const BrowserWindow = remote.BrowserWindow
-// const Menu = remote.require('menu')
-const ipc = electron.ipcRenderer
-
 export default {
   components: {
     Hello
